8eed16d281156c46f890b1728a8c119c533fec0f
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-11-30 17:50:47 +0300

    Create week8.cpp

---
+++ week8.cpp
@@ -0,0 +1,49 @@
+// Create a Strong Password
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+// Function Defining
+// This function gets the parameter that is integer varible
+string generatePassword(int length);
+// This function doesn't varible
+char getRandomChar();
+
+int main() {
+    // Seed setting for random number generation
+    srand(time(0));
+
+    // Determine password length
+    int passwordLength;
+    cout << "Şifre uzunluğunu belirleyin: ";
+    // We get the password length from the user
+    cin >> passwordLength;
+
+    // Create the Strong Password
+    string password = generatePassword(passwordLength);
+
+    // Print the generated password on the screen
+    cout << "Created Password: " << password << endl;
+
+    return 0;
+}
+
+// The Function that Generates Strong Password 
+string generatePassword(int length) {
+    string strongPassword;
+
+    for (int initial = 0; initial < length; ++initial) {
+        strongPassword += getRandomChar();
+    }
+    
+    return strongPassword;
+}
+
+// Rastgele karakter üreten fonksiyon
+char getRandomChar() {
+    const string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.!@#$%^&*()_+";
+    int randomCharacter = rand() % characters.length();
+    
+    return characters[randomCharacter];
+}
