700b520b23dc1886ac4a864d091652e017ea410e
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-12-07 18:04:52 +0300

    Update week9.cpp

--- week9.cpp
+++ week9.cpp
@@ -1,3 +1,5 @@
+// Example 9.1
+
 #include <iostream>
 #include <string>
 
@@ -47,3 +49,82 @@ int main() {
     // Nesnenin ömrü sona erdiğinde destructor çağrılır
     return 0;
 }
+
+
+// Example 9.2
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Araba {
+public:
+    // Default constructor (Parametre almayan constructor)
+    Araba() {
+        cout << "Default constructor çağrıldı." << endl;
+        marka = "Bilinmiyor";
+        model = "Bilinmiyor";
+        yil = 0;
+    }
+
+    // Parametre alan constructor
+    Araba(string markaAdi, string modelAdi, int uretimYili) {
+        cout << "Parametre alan constructor çağrıldı." << endl;
+        marka = markaAdi;
+        model = modelAdi;
+        yil = uretimYili;
+    }
+
+    // Diğer üye fonksiyonlar
+    void BilgileriGoster() const {
+        cout << "Marka: " << marka << ", Model: " << model << ", Yıl: " << yil << endl;
+    }
+
+    // Bilgileri ayarla fonksiyonu
+    void BilgileriAyarla(string yeniMarka, string yeniModel, int yeniYil) {
+        marka = yeniMarka;
+        model = yeniModel;
+        yil = yeniYil;
+        cout << "Bilgiler güncellendi." << endl;
+    }
+
+    // Üretim yılı güncelle fonksiyonu
+    void UretimYiliGuncelle(int yeniYil) {
+        yil = yeniYil;
+        cout << "Üretim yılı güncellendi." << endl;
+    }
+
+private:
+    string marka;
+    string model;
+    int yil;
+};
+
+int main() {
+    // Default constructor çağrılır
+    Araba araba1;
+
+    // Parametre alan constructor çağrılır
+    Araba araba2("Toyota", "Corolla", 2020);
+
+    // Bilgileri göster
+    cout << "Araba 1 Bilgileri: ";
+    araba1.BilgileriGoster();
+
+    cout << "Araba 2 Bilgileri: ";
+    araba2.BilgileriGoster();
+
+    // Bilgileri ayarla ve tekrar göster
+    araba1.BilgileriAyarla("Honda", "Civic", 2022);
+    cout << "Araba 1 Güncellenmiş Bilgileri: ";
+    araba1.BilgileriGoster();
+
+    // Üretim yılını güncelle ve tekrar göster
+    araba2.UretimYiliGuncelle(2021);
+    cout << "Araba 2 Güncellenmiş Bilgileri: ";
+    araba2.BilgileriGoster();
+
+    return 0;
+}
+
